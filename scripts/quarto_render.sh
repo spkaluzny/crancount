5192dc531a3cb95c672ee21cec9438896f065004
spkaluzny <spkaluzny@gmail.com>
2022-06-14 19:12:31 -0700

    Need to set path for cron job

--- scripts/quarto_render.sh
+++ scripts/quarto_render.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+PATH=/usr/local/bin:${PATH}
+export PATH
 DIR=/home/spk/DATA/crancount/reports
 cd $DIR
 rm -f cran_report.html
-quarto render cran_report.qmd
+/usr/local/bin/quarto render cran_report.qmd
 if [ -f cran_report.html ]; then
   cp -p cran_report.html ~/public_html/reports/
 else
